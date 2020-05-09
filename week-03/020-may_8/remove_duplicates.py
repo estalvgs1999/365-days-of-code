9ac310ad3696619af13709a0521ed69b444f3ec8
Esteban Alvarado <estalvgs1999@gmail.com>
2020-05-09 01:06:13 -0600

    Remove duplicates
    + i want it greener

--- week-03/020-may_8/remove_duplicates.py
+++ week-03/020-may_8/remove_duplicates.py
@@ -5,9 +5,6 @@
 #           REMOVE DUPLICATES
 #  Write code to remove duplicates from
 #  an unsorted linked list.
-#  FOLLOW UP
-#  How would you solve this problem if a
-#  temporary buffer is not allowed?
 #  
 #  Question 2.1 | ctci 6th edition
 #
